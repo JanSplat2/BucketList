33dfec72f1d4aef16aeb1d3606e02481bd3f636d
JanSplat2 <jan.splat@outlook.de>
2025-10-21 14:33:16 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Dittrich, Jan - Student on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Dittrich, Jan - Student on 10/21/25.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
